9e34dad69b9310cd26f22fe1dc517d2f1937cf10
ellimac <geoffroycamille3@gmail.com>
2025-07-26 15:17:04 +0200

    fix(dunst): notifications when changing layout

--- scripts/layout.sh
+++ scripts/layout.sh
@@ -4,7 +4,8 @@ if [ -z ${active_keyboard} ]; then
   echo 'Keyboard not found, exiting'
   exit 2
 fi
-key=`hyprctl devices | grep -oP 'active keymap: \K\w+' | tail -1`
-hyprctl notify 1 1000 "rgb(ff1ea3)" "Change keyboard: " $key
 
 hyprctl --instance "${HYPRLAND_INSTANCE_SIGNATURE}" switchxkblayout ${active_keyboard} next
+
+key=`hyprctl devices | grep -oP 'active keymap: \K\w+' | tail -1`
+notify-send -u low  $key
