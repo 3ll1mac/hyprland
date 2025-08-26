66bced59d220295c695e176410bbc510fcd8f5e6
ellimac <geoffroycamille3@gmail.com>
2025-08-27 00:22:27 +0200

    feat(notify): added notification for low batteri

---
+++ scripts/batterie.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+status=`cat /sys/class/power_supply/BAT0/status`
+current_charge=`cat /sys/class/power_supply/BAT0/charge_now`
+threshold_charge=`cat /sys/class/power_supply/BAT0/alarm`
+
+echo $status $current_charge $threshold_charge
+
+if [ $status = "Discharging" -a $current_charge -le $threshold_charge ]
+then
+	notify-send -u critical  "Batterie low"
+fi
+
